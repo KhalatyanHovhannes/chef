535495a4ecaf2b54d020a67a461f348e12fdb0f9
Hovhannes Khalatyan <Hovhannes_Khalatyan@epam.com>
2021-01-21 00:02:11 +0400

    fluentd install cookbook

--- cookbooks/Fluentd/recipes/default.rb
+++ cookbooks/Fluentd/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+# Install fluentd
+package "fluentd" do
+    action :install
+end
+# Enable service fluentd
+service "fluentd" do
+    action [:enable, :start]
+end
