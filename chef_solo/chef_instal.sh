06dfdd488a52947f777a45cbc147c2849a8b01c1
Hovhannes Khalatyan <Hovhannes_Khalatyan@epam.com>
2021-01-21 12:08:57 +0400

    bash

---
+++ chef_solo/chef_instal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# chef install&config
+yum install wget -y
+wget -O- https://opscode.com/chef/install.sh | sudo bash -y
+mkdir /etc/chef-solo
+mkdir /etc/chef-solo/cookbooks
+mkdir /etc/chef-solo/data_bags
+mkdir /etc/chef-solo/cache
+mkdir /etc/chef-solo/roles
+cd /etc/chef-solo
+cat <<EOT >> solo.rb
+echo cookbook_path [
+               '/etc/chef-solo/cookbooks',
+              ]
+data_bag_path '/etc/chef-solo/data_bags'
+file_cache_path '/etc/chef-solo/cache'
+role_path '/etc/chef-solo/roles' > solo.rb
+EOT
